6e157acd31c5460f2415e2756829a0aa02a93c2e
sumantmurke <sumantmurke@gmail.com>
2015-04-18 22:43:52 -0700

    running change

--- src/main/java/com/cloudigrate/facade/SesConfig.java
+++ src/main/java/com/cloudigrate/facade/SesConfig.java
@@ -15,12 +15,12 @@ private static String mySecretId;
 private static String myBucketName;
 
 public static String getMyAccessId() {
-	myAccessId= "AKIAIOYSXH464CBNEDMQ";
+	myAccessId= "";
 	return myAccessId;
 }
 
 public static String getMySecretId() {
-	mySecretId="IVp1821X7LY+H/hdjbK9T4HTqOLGrBN7/EmB7sNL"; 
+	mySecretId=""; 
 	return mySecretId;
 }
 
